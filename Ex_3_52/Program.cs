fc2f68149d431e30acb800a19334f7ae93cf4efc
VadimShapovalov <kidav_21@mail.ru>
2022-08-14 19:03:15 +0300

    Нахождение среднего арифметического в каждом столбце массива

---
+++ Ex_3_52/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:*/
+int[,] CreateAndPrintArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return array;
+}
+void FindAverageInColumn(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        double average = sum / array.GetLength(0);
+        Console.Write(Math.Round(average, 2) + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+FindAverageInColumn(CreateAndPrintArray(row, col));
